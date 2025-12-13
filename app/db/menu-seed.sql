ad492e26f93a481fa62d3a0aea92232e28106f9a
idli-dev <241423031+idli-dev@users.noreply.github.com>
2025-12-13 18:52:18 +0530

    whole menu ready to be seeded

---
+++ app/db/menu-seed.sql
@@ -0,0 +1,307 @@
+INSERT INTO
+    menu (item_name, price)
+VALUES
+    -- ==================================================
+    -- FRESH JUICES
+    -- ==================================================
+    ('Passion Fruit Juice', 80),
+    ('Apple Juice', 90),
+    ('Pomegranate Juice', 90),
+    ('Chiku Juice', 80),
+    ('Carrot Juice', 60),
+    ('Pineapple Juice', 70),
+    ('Orange Juice', 70),
+    ('Musambi Juice', 70),
+    ('Grape Juice', 70),
+    ('Cocktail Juice', 90),
+    ('Guava Juice', 80),
+    ('Watermelon Juice', 50),
+    ('Strawberry Juice', 130),
+    ('Papaya Juice', 60),
+    ('Mango Juice', 80),
+    -- ==================================================
+    -- SPECIAL LIME
+    -- ==================================================
+    ('Fresh Lime', 20),
+    ('Grape Lime', 30),
+    ('Mint Lime', 30),
+    ('Ginger Lime', 30),
+    ('Magic Lime', 40),
+    ('Orange Lime', 35),
+    ('Kashmiri Lime', 40),
+    ('French Lime', 40),
+    ('Pineapple Lime', 40),
+    -- ==================================================
+    -- SMOOTHIES
+    -- ==================================================
+    ('Mango Smoothie', 90),
+    ('Strawberry Smoothie', 130),
+    ('Chocolate Smoothie', 90),
+    ('Apple Smoothie', 100),
+    ('Bake Day Special Smoothie', 120),
+    -- ==================================================
+    -- MOJITOS
+    -- ==================================================
+    ('Mint Mojito', 70),
+    ('Lemon Mint Mojito', 80),
+    ('Orange Mojito', 80),
+    ('Blue Curacao Mojito', 90),
+    ('Passion Fruit Mojito', 90),
+    ('Green Apple Mojito', 100),
+    ('Guava Mojito', 90),
+    ('Strawberry Mojito', 90),
+    ('Watermelon Mojito', 80),
+    ('420 Paan Mojito', 120),
+    ('Whiskey Mojito', 120),
+    ('Pina Colada', 120),
+    -- ==================================================
+    -- HOT BEVERAGES
+    -- ==================================================
+    ('Tea', 15),
+    ('Masala Tea', 20),
+    ('Lemon Tea', 15),
+    ('Ginger Tea', 15),
+    ('Coffee', 20),
+    ('Boost', 20),
+    ('Horlicks', 30),
+    ('Hot Chocolate', 70),
+    -- ==================================================
+    -- COLD BEVERAGES
+    -- ==================================================
+    ('Lemon Iced Tea', 80),
+    ('Peach Iced Tea', 80),
+    ('Mint Iced Tea', 80),
+    ('Iced Black Coffee', 80),
+    ('Lemon Soda', 20),
+    ('Sp Nannari Sarbath', 30),
+    -- ==================================================
+    -- ICE CREAM SCOOPS
+    -- ==================================================
+    ('Vanilla Ice Cream', 35),
+    ('Strawberry Ice Cream', 35),
+    ('Pista Ice Cream', 35),
+    ('Mango Ice Cream', 35),
+    ('Chocolate Ice Cream', 40),
+    ('Spanish Delight Ice Cream', 45),
+    ('Tender Coconut Ice Cream', 45),
+    ('Kulfi Ice Cream', 45),
+    ('Chikku Ice Cream', 35),
+    ('Guava Ice Cream', 40),
+    ('Black Currant Ice Cream', 40),
+    ('Fig n Honey Ice Cream', 45),
+    ('Passion Fruit Ice Cream', 40),
+    ('Butterscotch Ice Cream', 40),
+    -- ==================================================
+    -- AVIL MILK
+    -- ==================================================
+    ('Avil Milk Kids Special', 30),
+    ('Avil Milk Dora Kids', 60),
+    ('Avil Milk Munch Tower', 80),
+    ('Avil Milk KitKat Tower', 80),
+    ('Avil Milk Snickers Tower', 100),
+    -- ==================================================
+    -- DIET
+    -- ==================================================
+    ('Diet Normal', 40),
+    ('Diet Nuts', 90),
+    ('Diet Fruit', 80),
+    ('Diet Nuts Fruits', 110),
+    -- ==================================================
+    -- SUPREME (AVIL MILK)
+    -- ==================================================
+    ('Supreme Coffee', 60),
+    ('Supreme Royal', 70),
+    ('Supreme Irish', 90),
+    ('Supreme Chikku Special', 80),
+    ('Supreme Fruit Special', 80),
+    ('Supreme Black Currant', 80),
+    ('Supreme Irish Cadbury', 100),
+    ('Supreme Dry Fruit Spl', 90),
+    ('Supreme Chocolate', 80),
+    ('Supreme Planet Dry Fruit', 120),
+    ('Supreme Butter Dry Fruit', 100),
+    ('Supreme Butterscotch', 100),
+    ('Supreme Kulfi', 100),
+    ('Supreme Oreo Dry Fruit', 110),
+    ('Supreme Tender Coconut', 100),
+    ('Supreme Badam', 100),
+    ('Supreme Fig Badam', 110),
+    ('Supreme Fig Honey', 120),
+    ('Supreme Dates', 100),
+    ('Supreme Italian', 90),
+    ('Supreme Irani', 100),
+    ('Supreme Russian', 100),
+    ('Supreme Oreo', 100),
+    ('Supreme Nut Banana', 100),
+    ('Supreme Malgova Mango', 110),
+    -- ==================================================
+    -- SPECIAL (AVIL MILK)
+    -- ==================================================
+    ('Special Avil Milk', 60),
+    ('Special Butterscotch', 70),
+    ('Special Fig Nannari', 60),
+    ('Special White Chocolate', 70),
+    ('Special Chocolate', 70),
+    ('Special Strawberry', 70),
+    ('Special Guava', 70),
+    ('Special Pista', 80),
+    ('Special Caramel', 80),
+    ('Special Boost', 50),
+    ('Special Fruits', 60),
+    ('Special Horlicks', 50),
+    ('Special Boost Plus', 60),
+    -- ==================================================
+    -- SHAKES
+    -- ==================================================
+    ('Sharjah Shake', 60),
+    ('Snickers Shake', 100),
+    ('Kit Kat Shake', 100),
+    ('Oman Shake', 80),
+    ('Oreo Shake', 100),
+    ('Kulfi Shake', 90),
+    ('Cold Coffee', 90),
+    ('Black Currant Shake', 90),
+    ('Sharjah Choco Shake', 80),
+    ('Five Star Shake', 90),
+    ('Shamam Shake', 80),
+    ('Galaxy Shake', 100),
+    ('Arabian Shake', 100),
+    ('Cocktail Shake', 90),
+    ('Strawberry Shake', 90),
+    ('Butterscotch Shake', 90),
+    ('Chocolate Shake', 80),
+    ('Fig n Honey Shake', 80),
+    ('Irani Musk Shake', 90),
+    ('Irish Cadbury Shake', 100),
+    ('Spanish Delight Shake', 90),
+    ('Vanilla Shake', 80),
+    ('Chikku Shake', 80),
+    ('Mango Shake', 80),
+    ('Saudi Shake', 80),
+    ('Pista Shake', 80),
+    ('Apple Fruit Shake', 100),
+    ('Badam Shake', 100),
+    ('Fig Badam Shake', 110),
+    ('Italian Shake', 90),
+    ('Irani Shake', 90),
+    ('Russian Shake', 110),
+    ('Nut Banana Shake', 100),
+    ('Peanut Butter Shake', 90),
+    -- ==================================================
+    -- PREMIUM SHAKES
+    -- ==================================================
+    ('Aurora Shake', 140),
+    ('Bake n Wake Shake', 140),
+    ('Pastry Shake', 140),
+    ('Tender Coconut Shake', 120),
+    ('King Kong Shake', 160),
+    -- ==================================================
+    -- FALOODA
+    -- ==================================================
+    ('Fancy Falooda', 90),
+    ('Bombay Falooda', 120),
+    ('American Falooda', 120),
+    ('Royal Falooda', 130),
+    ('Arabic Temptation', 160),
+    ('Italian Temptation', 140),
+    ('Bake n Wake Falooda', 160),
+    ('Calcutta Street Falooda', 130),
+    ('Monster Falooda', 250),
+    -- ==================================================
+    -- FRUIT SALAD & SUNDAES
+    -- ==================================================
+    ('Fruit Salad Lite', 80),
+    ('Fruit Salad Pro', 100),
+    ('Fruits Overloaded', 110),
+    ('Brownie with Ice Cream', 120),
+    ('Dark Temptation', 130),
+    ('Bake n Wake Fruit Salad', 130),
+    ('Belgium Chips Sundae', 100),
+    ('Oreo Desperado', 110),
+    ('Mangomalwa', 100),
+    ('Bake n Cake', 120),
+    ('Gulab Jamun with Ice Cream', 80),
+    -- ==================================================
+    -- SHAWARMA & WRAPS
+    -- ==================================================
+    ('Jumbo Shawarma', 140),
+    ('Jumbo Cheese Shawarma', 160),
+    ('Rumali Roti Roll', 110),
+    ('Kubbus Roll', 80),
+    ('Plate Shawarma Rumali', 160),
+    ('Plate Shawarma Kubbus', 120),
+    ('Mini Wrap (Kubbus)', 80),
+    ('Crispy Wrap Individual', 120),
+    ('Crispy Wrap Combo', 180),
+    ('Beef Thawa Roll', 130),
+    ('Chicken Thawa Roll', 120),
+    -- ==================================================
+    -- MINI BITES & FRIES
+    -- ==================================================
+    ('Chick Pop', 100),
+    ('Chicken Nuggets', 100),
+    ('Veg Nuggets', 90),
+    ('French Fries Small', 50),
+    ('French Fries Medium', 80),
+    ('French Fries Large', 130),
+    ('Veg Loaded Fries', 180),
+    ('Chicken Loaded Fries', 220),
+    -- ==================================================
+    -- RICE, NOODLES & CURRIES
+    -- ==================================================
+    ('Chicken Dum Biriyani', 140),
+    ('Egg Biriyani', 120),
+    ('Beef Biriyani', 150),
+    ('Prawns Biriyani', 110),
+    ('Veg. Biriyani', 60),
+    ('Neychor', 140),
+    ('Chicken Fried Rice', 100),
+    ('Veg. Fried Rice', 160),
+    ('Mixed Fried Rice', 120),
+    ('Egg Fried Rice', 120),
+    ('Veg noodles', 130),
+    ('Chicken noodles', 150),
+    ('Egg noodles', 130),
+    ('Schezwan noodles', 160),
+    -- ==================================================
+    -- CHICKEN & BEEF
+    -- ==================================================
+    ('Chicken Curry', 100),
+    ('Chicken Roast', 120),
+    ('Pepper Chicken', 150),
+    ('Garlic Chicken', 150),
+    ('Chilly Chicken', 150),
+    ('Chicken Chukka', 140),
+    ('Chicken Kondattam', 150),
+    ('Butter Chicken', 160),
+    ('Chicken - 65', 120),
+    ('Kadai Chicken', 150),
+    ('Beef Roast', 120),
+    ('Kozhikodan Beef Chilly', 140),
+    ('Kozhikodan Beef Stew', 150),
+    ('Beef Thengakothittathu', 130),
+    ('Chilly Beef', 140),
+    ('Beef Dry Fry', 150),
+    ('Beef Chukka', 150),
+    ('Beef Kondattam', 150),
+    ('Beef Fry', 140),
+    ('Beef Curry', 100),
+    -- ==================================================
+    -- BREAD & VEG ITEMS
+    -- ==================================================
+    ('Porotta', 12),
+    ('Chapathi', 10),
+    ('Pathiri', 8),
+    ('Rumali Roti', 15),
+    ('Kubbus', 10),
+    ('Nool Porotta [Live]', 20),
+    ('Kizhi Porotta [Chicken]', 120),
+    ('Kizhi Porotta [Beef]', 130),
+    ('Kothu Porotta [Chicken]', 120),
+    ('Kothu Porotta [Beef]', 130),
+    ('Paneer Butter Masala', 130),
+    ('Kadai Paneer', 130),
+    ('Veg. Kurma', 120),
+    ('Chilly Gopi', 120),
+    ('Gopi Manchurian', 120),
+    ('Chilly Paneer', 130);
